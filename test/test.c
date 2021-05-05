45a1cdede381f846245ae27818583ce20c8780d0
“DESMARECAUX <[romain.desmarecaux@etu.univ-amu.fr]>
2021-05-05 23:31:39 +0200

    Modification du test/test.c.

--- test/test.c
+++ test/test.c
@@ -1,7 +1,18 @@
+#include "db.h"
+#include "vector.h"
+
+#include "test_harness/test_harness.h"
+
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Valeurs pour le harnais de test spécifiques à ce programme.
+int const tests_total = 0;
+int const test_column_width = 80;
 
-int main(void){
-    return 0;
+int main()
+{
+    return tests_executed - tests_successful;
 }
