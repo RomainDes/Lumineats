7e74dab0c169b07b4efd6a421916a263620e3cd8
Guillaume <[guillaume.polverelli@gmail.com]>
2021-05-05 22:52:20 +0200

    Changement des types int en size_t dans la structure client de client.h

--- lib/client.h
+++ lib/client.h
@@ -1,8 +1,8 @@
 typedef struct client              //Structure pour la table client
 {
-    int id;
+    size_t id;
     char nom[TAILLE_CHAMP_NOM];
-    int code_postal;
+    size_t code_postal;
     char telephone[10];
     float solde;
     char mot_de_passe[TAILLE_CHAMP_MDP];
