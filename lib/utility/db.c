6ee4e8239bdea9358524ec7662a2ddb5ccb89302
Guillaume <[guillaume.polverelli@gmail.com]>
2021-05-16 16:56:48 +0200

    Ecriture des fonctions dans db.c pour pour pouvoir lire et écrire dans les tables de client, livreur et restaurant

--- lib/utility/db.c
+++ lib/utility/db.c
@@ -9,41 +9,89 @@
 
 
 
-vector lecture_table(
+vector lecture_table_client(
     FILE* file)
 {
     float const growth_factor_doubling = 2.;
 
-    docteur doctor;
+    client user;
 
-    vector base_donnee = make_vector(sizeof(doctor),0, growth_factor_doubling);
+    vector base_donnee = make_vector(sizeof(user),0, growth_factor_doubling);
     
-    while(fscanf(file, "%zu,%127[^,],%i,%zu", &doctor.index, doctor.nom, &doctor.specialites, &doctor.index_hopital) == 4)
+    while(fscanf(file, "%zu,%127[^,],%zu,%127[^,],%f,%127[^,]", &user.id, user.nom, &user.code_postal, user.telephone, user.solde, user.mot_de_passe) == 6)
     {
-        push_back(&base_donnee, &doctor);
+        push_back(&base_donnee, &user);
     }
     return base_donnee;
 }
 
-void ecriture_table(
+vector lecture_table_livreur(
+    FILE* file)
+{
+    float const growth_factor_doubling = 2.;
+
+    livreur livreur;
+
+    vector base_donnee = make_vector(sizeof(livreur),0, growth_factor_doubling);
+    
+    while(fscanf(file, "%zu,%127[^,],%zu,%127[^,],%f,%127[^,]", &livreur.id, livreur.nom, &livreur.code_postal, livreur.telephone, livreur.solde, livreur.mot_de_passe) == 6)
+    {
+        push_back(&base_donnee, &livreur);
+    }
+    return base_donnee;
+}
+
+vector lecture_table_restaurant(
+    FILE* file)
+{
+    float const growth_factor_doubling = 2.;
+
+    restaurant restau;
+
+    vector base_donnee = make_vector(sizeof(restau),0, growth_factor_doubling);
+    
+    while(fscanf(file, "%zu,%127[^,],%127[^,],%zu,%127[^,],%127[^,],%zu,%f", &restau.id, restau.nom, restau.mdp, &restau.cp, restau.tel, restau.type, &restau.menu, restau.solde) == 8)
+    {
+        push_back(&base_donnee, &restau);
+    }
+    return base_donnee;
+}
+
+void ecriture_table_client(
     FILE* file,
     vector const* db)
 {
         for(iterator i = begin(db), e = end(db); compare(i, e) != 0; increment(&i, 1))
         {
-            docteur const* doctor = (docteur*)i.element;
-            fprintf(file, "%zu,%s,%i,%zu\n", doctor->index, doctor->nom, doctor->specialites, doctor->index_hopital);
+            client const* user = (client*)i.element;
+            fprintf(file, "%zu,%127[^,],%zu,%127[^,],%f,%127[^,]\n", user->id, user->nom, user->code_postal, user->telephone, user->solde, user->mot_de_passe);
         }
 
 
 }
 
-bool est_generaliste(
-    void const* d)
+void ecriture_table_livreur(
+    FILE* file,
+    vector const* db)
 {
+        for(iterator i = begin(db), e = end(db); compare(i, e) != 0; increment(&i, 1))
+        {
+            livreur const* livreur = (livreur*)i.element;
+            fprintf(file, "%zu,%127[^,],%zu,%127[^,],%f,%127[^,]\n", livreur->id, livreur->nom, livreur->code_postal, livreur->telephone, livreur->solde, livreur->mot_de_passe);
+        }
 
-    enum specialite_t generaliste = 0x8;
-    
-    if((char)generaliste <= *(char*)(d+44) && *(char*)(d+44) != (char)16 )    return true;//Je ne sais pas comment savoir s'il est MEDECINE_GENERALE
-    else    return false;
+
+}
+
+void ecriture_table_restaurant(
+    FILE* file,
+    vector const* db)
+{
+        for(iterator i = begin(db), e = end(db); compare(i, e) != 0; increment(&i, 1))
+        {
+            restaurant const* restau = (restaurant*)i.element;
+            fprintf(file, "%zu,%127[^,],%127[^,],%zu,%127[^,],%127[^,],%zu,%f\n", restau->id, restau->nom, restau->mdp, restau->cp, restau->tel, restau->type, restau->menu, restau->solde);
+        }
+
+
 }
