45490ac60e4306a0a3d13cf3820ac54c09ec9d0c
POLVERELLI Guillaume <p18000097@V-PJ-30-015.salsa.univ-amu.fr>
2021-05-21 17:10:41 +0200

    Implémentation des fonctions liés aux comptes (ajout de nouvelles fonctions connecter_compte et trouver_compte) ainsi que les fonctions consulter et créditer le compte. Il faudra maintenant que ces fonctions passent les tests.

--- lib/client.c
+++ lib/client.c
@@ -12,6 +12,102 @@ size_t counter_index_client(){
                          /*----------COMPTE----------*/
 
 
+//Fonction utilitaire permettant de trouver dans la base de donnee
+//les informations du compte avec l'id id
+iterator trouver_client_avec_id(int id){
+    FILE* fichierclient;
+    fichierclient = fopen("./database/client.csv", "r");
+    vector dbclient = lecture_table_clients(fichierclient);
+
+    iterator actu;
+    int trouve = 0;
+    client * client_dans_bd;
+
+    actu = begin(&dbclient);
+
+    while(trouve == 0){
+        client_dans_bd = (struct client*) actu.element;
+        if(client_dans_bd->id == id){
+            trouve = 1;
+        }
+        increment(&actu, 1);
+    }
+
+    fclose(fichierclient);
+    return actu;
+}
+
+
+//Permet a un client de se connecter a son compte, renvoie l'id du compte ou on se 
+//connecte si on y parvient, 0 sinon
+int connecter_compte_client(){
+    //On demande au client son nom, puis on ouvre la db client, on demande ensuite
+    //a l'utilisateur d'entrer son mot de passe et on verifie si il correspond au mdp
+    //present dans la db a la ligne correspondant au nom entré.
+    //Si le nom entré n'est pas dans la db on redemande, idem pour mdp si il ne correspond pas
+    //On laisse 3 essais pour rentrer le mot de passe
+    int valid = 0;
+    char username[TAILLE_NOM];
+    char password[TAILLE_MDP];
+    client* client_dans_bd;
+    FILE * fichierclient;
+    iterator actu, fin;
+    int essai = 3;
+    int res;
+
+    fichierclient = fopen("./database/client.csv", "r");
+    vector dbclient = lecture_table_clients(fichierclient);
+
+    //On demande le nom d'utilisateur et vérifie qu'il est bien dans la db
+    while (valid == 0){
+
+        printf("Veuillez entrer votre nom d'utilisateur : ");
+        scanf("%s \n", username);
+
+        actu = begin(&dbclient);
+        fin = end(&dbclient);
+
+        while(actu.element != fin.element && valid == 0){
+            client_dans_bd = (struct client*) actu.element;
+            if (client_dans_bd->nom == username){
+                valid = 1;
+            }
+            else{
+                increment(&actu, 1);
+            }
+        }
+        //A la fin de cette boucle, on aura dans client_dans_db la struct client correspondant au compte auquel on veut
+        //se connecter, on aura donc plus besoin de la chercher
+        if(valid == 0){
+            printf("Nom d'utilisateur invalide, veuillez réessayer.\n");
+        }
+    }
+    //On remet valid a 0 pour le test du mot de passe
+    valid = 0;
+    while(essai > 0 && valid == 0){
+        printf("Veuillez entrer votre mot de passe. Il vous reste %i essais\n", essai);
+        scanf("%s \n", password);
+        if (client_dans_bd->mdp == password){
+            valid = 1;
+        }
+        else{
+            printf("Mot de passe incorrect, veuillez reessayer.\n");
+            essai -= 1;
+        }
+    }
+    //On recupere l'id du compte si on a entré le bon mdp
+    if (valid == 1){
+        res = client_dans_bd->id;
+    }
+    else{
+        res = 0;
+    }
+
+    fclose(fichierclient);
+    return res;
+}
+
+
 
 //Permet a un client de se creer un compte en entrant toutes ses informations
 void creer_compte_client(){
@@ -81,14 +177,106 @@ void creer_compte_client(){
 
 //Permet a un client de supprimer son compte et toutes les information y etant contenues
 
-void supprimer_compte_client(){
+void supprimer_compte_client(int id){
+    FILE* fichierclient;
+    fichierclient = fopen("./database/client.csv","r+");
+    vector dbclient = lecture_table_clients(fichierclient);
 
+    iterator iterateur;
+
+    iterateur = trouver_client_avec_id(id);
+
+    erase(&dbclient, iterateur);
+
+    return;
 }
 
 //Permet à un client de modifier son profil (Code postal et téléphone)
 
-void modifier_profil_client(){
+//Deux fonctions pour cela 
 
+//Changer de code postal
+void modifier_cp_client(int id){
+    //On ouvre le fichier csv des clients, on récupere la ligne correspondant a 
+    //l'index du compte ou on est connecté, on récupere le code postal
+    //On remplace avec le nouveau code postal
+
+    FILE* fichierclient;
+    fichierclient = fopen("./database/client.csv","r+");
+    vector dbclient = lecture_table_clients(fichierclient);
+
+    client* client_connecte;
+    iterator iterateur;
+    char cp_actu[5];
+
+    iterateur = trouver_client_avec_id(id);
+    client_connecte = (struct client*) iterateur.element;
+
+    printf("Veuillez entrer un nouveau code postal\n");
+    scanf("%s", cp_actu);
+    for(int i = 0; i < 5; i++){
+        client_connecte->code_postal[i]=cp_actu[i];
+    }
+
+    set(iterateur, (void *)client_connecte);
+    ecriture_table_clients(fichierclient, &dbclient);
+
+    fclose(fichierclient);
+
+    return;
+}
+
+
+//Changer de numéro de téléphone
+void modifier_tel_client(int id){
+
+    FILE* fichierclient;
+    fichierclient = fopen("./database/client.csv","r+");
+    vector dbclient = lecture_table_clients(fichierclient);
+
+    client* client_connecte;
+    iterator iterateur;
+    char tel_actu[14];
+
+    iterateur = trouver_client_avec_id(id);
+    client_connecte = (struct client*) iterateur.element;
+
+    printf("Veuillez entrer un nouveau numéro de téléphone\n");
+    scanf("%s", tel_actu);
+    for(int i = 0; i < 14; i++){
+        client_connecte->tel[i]=tel_actu[i];
+    }
+
+    set(iterateur, (void *)client_connecte);
+    ecriture_table_clients(fichierclient, &dbclient);
+
+    fclose(fichierclient);
+
+    return;
+}
+
+//Permet a un client de modifier les differents elements vu ci dessus
+void modifier_compte_client(int id){
+    //On combine les deux fonctions au dessus en demandant a l'utilisateur ce qu'il
+    //souhaite  modifier
+    int value;
+    printf("Veuillez taper 1 ou 2 selon ce que vous souhaitez modifier : \n1) Code postal\n2) Numero de tel");
+    scanf("\n%i\n",&value);
+
+    switch (value)
+    {
+    case 1:
+        modifier_cp_client(id);
+        break;
+    case 2:
+        modifier_tel_client(id);
+        break;
+    default:
+        printf("Valeur incorrecte, veuillez recommencer\n");
+        break;
+    }
+
+    return;
 }
 
 
@@ -96,16 +284,52 @@ void modifier_profil_client(){
 
 
 
-//Permet  à un client de consulter son solde
+//Permet à un client de consulter son solde
+void consulter_solde_client(int id){
+    //On ouvre la bd client et on recupère la valeur du solde a la ligne du compte
+    //auquel on est connecté
 
-void consulter_solde_client(){
+    iterator iterateur;
+    client * client_dans_bd;
 
+    iterateur = trouver_client_avec_id(id);
+
+    client_dans_bd = (struct client*) iterateur.element;
+    
+    printf("Le solde sur ce compte est de %f euros", client_dans_bd->solde);
+
+    return;
 }
 
 //Permet à un client de créditer son solde d'un montant
 
-void crediter_solde_client(){
+void crediter_solde_client(int id){
+    //On ouvre la bd client et on demande à l'utilisateur le montant qu'il veut crediter 
+    //sur son solde a la ligne du compte auquel on est connecté
 
+    FILE* fichierclient;
+    fichierclient = fopen("./database/client.csv","r+");
+    vector dbclient = lecture_table_clients(fichierclient);
+
+
+    iterator iterateur;
+    client * client_connecte;
+
+    float nouv_solde;
+    iterateur = trouver_client_avec_id(id);
+
+    client_connecte = (struct client*) iterateur.element;
+    
+    printf("Indiquez le montant que vous voulez créditer sur votre compte\n");
+    scanf("%f", &nouv_solde);
+    client_connecte -> solde += nouv_solde;
+    
+    set(iterateur, (void *)client_connecte);
+    ecriture_table_clients(fichierclient, &dbclient);
+
+    fclose(fichierclient);
+
+    return;
 }
 
 //Permet de débiter le solde d'un client
